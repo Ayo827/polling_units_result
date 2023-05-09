4ce0b82551113f30cbb49fddf88a7fb117a4ef1c
Ayodeji Adebola <53396591+Ayo827@users.noreply.github.com>
2023-05-10 00:09:39 +0100

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-   baseURL: "https://polling-unit-server.herokuapp.com/"
+   baseURL: "https://polling-unit-server.onrender.com/"
 });
 
 export default instance;
